94de253c8260d2c8b44577e90a91857f05e1893b
Velociraptors-in-Space <320330a@gmail.com>
2022-08-17 21:08:08 -0400

    Refactoring

---
+++ servicable.py
@@ -0,0 +1,11 @@
+from car import Car
+
+class Servicable:
+    def __init__(self):
+        pass
+
+    """
+    Returns if a car needs servicing
+    """
+    def needs_service(Car):
+        return Car.needs_service()
